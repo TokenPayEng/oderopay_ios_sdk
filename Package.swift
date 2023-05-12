d047fbfe83088f7b9b6a0938b8e79d3b11b196ad
Kadir Guzel <kadirguzell@hotmail.com>
2023-05-13 02:56:08 +0300

    Package file configurated

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
                  name: "OderoPaySdk",
-                 url: "http://kftech.co/ios_sdk/OderoPaySdk.zip",
+                 url: "https://gitlab.com/kftech/oderopaysdk/-/raw/main/OderoPaySdk.zip?inline=false",
                  checksum: "7e6fac906b672a08151703422d678e0071b5645111e84fd5caff282a034e8b47"
              )
     ]
